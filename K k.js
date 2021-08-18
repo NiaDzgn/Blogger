b9b8ca26a5f3a43dadb1c682e1b0ed3d72affaf9
NiaDzgn <88774726+NiaDzgn@users.noreply.github.com>
2021-08-18 15:58:56 +0500

    Add files via upload

---
+++ K k.js
@@ -0,0 +1,10 @@
+'use strict';
+var _0x8dea = ['Created with <i style="color:#ff695d;" class="fa fa-heart"></i> by <a href="https://Fetney.blogspot.com/">Fetney</a>', "html", "display", "inline-block", "css", "#Fscontent", "length", "#Fscontent:visible", "href", "location", "https://Fetney.blogspot.com/", "ready"];
+$(document)[_0x8dea[11]](function () {
+    $(_0x8dea[5])[_0x8dea[4]](_0x8dea[2], _0x8dea[3])[_0x8dea[1]](_0x8dea[0]);
+    setInterval(function () {
+        if (!$(_0x8dea[7])[_0x8dea[6]]) {
+            window[_0x8dea[9]][_0x8dea[8]] = _0x8dea[10]
+        }
+    }, 3000)
+});
